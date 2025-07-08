f47ff95352c1c4c7e92d23bde5c70c349363a815
sk-sazid <1.sksazid@gmail.com>
2025-07-08 15:25:34 +0600

    Project Lombok (@Data annotation) That auto generate Getter, Setter, ToString etc.

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -1,6 +1,7 @@
 package net.engineeringdigest.journalApp.entity;
 
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Document(collection = "journal_entries")  // It mapped with row value of database
+@Data  // Project Lombok, That auto generate Getter, Setter, ToString etc. using @Data annotation.
 public class JournalEntry {
 
     @Id  // Make id as a primary key
@@ -15,36 +17,4 @@ public class JournalEntry {
     private String title;
     private String content;
     private LocalDateTime date;
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
 }
